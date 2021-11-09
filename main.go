8f9bd3a88ef92f7a1e7866b735ad07a09a7d3c54
ShwetaT0mar <36586308+ShwetaT0mar@users.noreply.github.com>
2021-11-09 18:24:46 +0530

    Slot with min waiting time, Preparation Time, Adding Clocks for slots

--- main.go
+++ main.go
@@ -3,9 +3,14 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"time"
 
 	"github.com/ShwetaT0mar/FoodDeliveryCalculator/service"
 )
+
+func init() {
+	Clock()
+}
 
 func main() {
 
@@ -22,6 +27,28 @@ func active(w http.ResponseWriter, r *http.Request) {
 	a := struct {
 		Status string
 	}{"Active"}
-
 	json.NewEncoder(w).Encode(a)
+}
+
+func Clock() {
+	go Timer(1)
+	go Timer(2)
+	go Timer(3)
+	go Timer(4)
+	go Timer(5)
+	go Timer(6)
+	go Timer(7)
+}
+
+func Timer(slot int) {
+
+	for {
+		t := service.GetSlotTime(slot)
+		if t != 0 {
+			time.Sleep(1 * time.Minute)
+			service.SlotStruct.Lock()
+			service.SlotStruct.AllSlots[slot]--
+			service.SlotStruct.Unlock()
+		}
+	}
 }
